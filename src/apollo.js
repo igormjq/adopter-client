eef62ac9481b6c67ee7193bfb406198f43481dad
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-05-15 17:44:29 -0300

    :lock: Add token to authorization header on request

--- src/apollo.js
+++ src/apollo.js
@@ -1,19 +1,31 @@
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
   uri: 'http://localhost:4000/',
 })
 
 // Cache implementation
 const cache = new InMemoryCache()
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache,
 })
 
