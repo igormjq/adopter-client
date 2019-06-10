6dc8c3f3bd6ebf75609a7aaa31e7ad4c1a51390a
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-06-10 15:38:18 -0300

    :sparkles: Add new route to create animals

---
+++ src/components/views/animals/Animal.create.vue
@@ -0,0 +1,15 @@
+<template>
+  <section id="animalCreate">
+    <container>
+      <h2>CRETE</h2>
+    </container>
+  </section>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ import Home from '../components/views/home/Home.index.vue'
 import AnimalHome from '../components/views/animals/Animal.vue';
 import Animals from '../components/views/animals/Animal.index.vue'
 import AnimalDetail from '../components/views/animals/Animal.detail.vue';
+import AnimalCreate from '../components/views/animals/Animal.create.vue';
 import Favorites from '../components/views/animals/Animal.favorites.vue'
 
 export default [
   { path: '/', component: Home },
   { path: '/animals', component: AnimalHome, children: [
       { path: '', component: Animals },
-      { path: ':id', component: AnimalDetail }
+      { path: 'new', component: AnimalCreate },
+      { path: ':id', component: AnimalDetail },
     ]
   },
   { path: '/favorites', component: Favorites }
