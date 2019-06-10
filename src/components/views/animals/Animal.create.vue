cec8f93a6768cfd0c760da33d3c13a39ee8b652a
Igor Marini Jaekel Quevedo <igormjq@gmail.com>
2019-06-10 20:36:15 -0300

    :heavy_plus_sign: Add Swiper lib

--- src/components/views/animals/Animal.create.vue
+++ src/components/views/animals/Animal.create.vue
@@ -1,19 +1,30 @@
 <template>
-  <section id="animalCreate">
-    <container class="flex-column">
-      <h2>Cadastrar novo amigo</h2>
-        <swiper :options="swiperOption">
-          <swiper-slide>
-            <card>
-              <div slot='content'>
-                <h1>aheuaseua</h1>
+  <section id="animalCreate" class="flex">
+    <container class="flex-column align-center justify-center">
+      <div class="wrapper">
+        <card>
+          <swiper :options="swiperOption" slot="content" class="swiper__progress">
+            <swiper-slide class="step flex flex-column">
+              <div class="step-title flex justify-center">
+                <span>Qual espécie do animal?</span>
               </div>
-            </card>
-          </swiper-slide>
-          <swiper-slide>Slide 2</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <div class="swiper-pagination" slot="pagination"></div>
-        </swiper>
+              <div class="step__options flex align-center">
+                <div class="option --full flex flex-column justify-center align-center">
+                  <div class="icon dog"></div>
+                  <h2>Cão</h2>
+                </div>
+                <div class="option --full flex flex-column justify-center align-center">
+                  <div class="icon cat"></div>
+                  <h2>Gato</h2>
+                </div>
+              </div>
+            </swiper-slide>
+            <swiper-slide>Slide 2</swiper-slide>
+            <swiper-slide>Slide 3</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+          </swiper>
+        </card>
+      </div>
     </container>
   </section>
 </template>
@@ -36,5 +47,19 @@
   }
 </script>
 <style lang="scss">
+  #animalCreate {
+    background-color: #eaebed;
+    .wrapper {
+      width: 35vw;
+      height: 50vh;
 
+      .card {
+        height: 100%;
+        border-radius: 0;
+        &__content {
+          height: 100%;
+        }
+      }
+    }
+  }
 </style>
